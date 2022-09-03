5cfb45255246f438ec40d6b0875a30351205773f
broc <brocgailit@gmail.com>
2022-09-03 18:01:35 +0300

    Move icon to document

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,11 +37,6 @@ function Home({ weddingDate, itinerary } : InferGetStaticPropsType<typeof getSta
       <Head>
         <title>{t('title')}</title>
         <meta name="description" content={t('title')} />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
 
       <main className={styles.main}>
